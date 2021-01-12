eb89b512f68a9e6213cda7ba07bad74b63c1f47b
gnda <kevin.nandacoumar@gmail.com>
2021-01-12 15:03:58 +0100

    Flask backend integration with docker

--- volumes/back/inventory/app.py
+++ volumes/back/inventory/app.py
@@ -1,14 +1,66 @@
-#! /usr/bin/python3
-from flask import Flask, Response, render_template, request, session, redirect, jsonify
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from marshmallow_sqlalchemy import ModelSchema
+from marshmallow import fields
 
-app = Flask(__name__, static_url_path="/static")
+mysql_username = "user"
+mysql_password = "test1234"
+mysql_host = "inventory_db"
+mysql_port = "3306"
+mysql_db = "inventory_db"
 
-@app.route("/")
-def getInventory():
-    """
-    Retrieve inventory
-    """
-    return jsonify({"id": 3, "content": "actual content"})
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://' + mysql_username + ':' + mysql_password + '@' + mysql_host + ':' + mysql_port + '/' + mysql_db
+db = SQLAlchemy(app)
 
-if __name__ == "__main__":
+
+@app.route('/')
+def hello_world():
+    return 'Hello World'
+
+class Product(db.Model):
+    __tablename__ = "product"
+    idproduct = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    description = db.Column(db.Text)
+    price = db.Column(db.Integer)
+
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
+    def __init__(self, name, description, price):
+        self.name = name
+        self.description = description
+        self.price = price
+
+    def __repr__(self):
+        return '' % self.id
+
+
+db.create_all()
+
+
+class ProductSchema(ModelSchema):
+    class Meta(ModelSchema.Meta):
+        model = Product
+        sqla_session = db.session
+
+    idproduct = fields.Integer(dump_only=True)
+    name = fields.String(required=True)
+    description = fields.String(required=True)
+    price = fields.Number(required=True)
+
+
+@app.route('/products', methods=['GET'])
+def get_all_products():
+    get_products = Product.query.all()
+    product_schema = ProductSchema(many=True)
+    products = product_schema.dump(get_products)
+    
+    return make_response(jsonify({"products": products}))
+
+
+if __name__ == '__main__':
     app.run("0.0.0.0", port=8000, debug=True)
--- volumes/front/src/index.js
+++ volumes/front/src/index.js
@@ -5,13 +5,22 @@ const fetch = require('node-fetch');
 const app = express();
 
 const inventory = 'http://inventory_back:8000/';
+const product = inventory + 'products';
 
 app.get('/', (req, res) => {
-    res.send('Accueils')
+    res.send('Accueil')
 })
 
 app.get('/inventory', (req, res) => {
     fetch(inventory)
+        .then(res => res.json())
+        .then(json => {
+            res.send(json)
+        });
+})
+
+app.get('/products', (req, res) => {
+    fetch(product)
         .then(res => res.json())
         .then(json => {
             res.send(json)
