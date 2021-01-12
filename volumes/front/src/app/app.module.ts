e17c1afb2fee3e9b214830cbce87d748711f04be
gnda <kevin.nandacoumar@gmail.com>
2021-01-12 16:39:33 +0100

    Removing Express front, using Angular instead

---
+++ volumes/front/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { DeleteProductModalComponent } from './components/delete-product-modal/delete-product-modal.component';
+import { AddOrEditProductModalComponent } from './components/add-or-edit-product-modal/add-or-edit-product-modal.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ShowProductComponent } from './components/show-product/show-product.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    DeleteProductModalComponent,
+    AddOrEditProductModalComponent,
+    ShowProductComponent
+  ],
+  imports: [
+    BrowserModule,
+    ClarityModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ volumes/front/src/app/services/categories.service.ts
@@ -0,0 +1,20 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { Response } from '../models/response';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriesService {
+
+  private baseUrl = `${environment.api+'category'+'?API_KEY='+environment.api_key}`;
+
+  constructor(private http: HttpClient) { }
+
+  getCategory(): Observable<Response>{
+    return this.http.get<Response>(this.baseUrl);
+  }
+
+}
