bef7841206e3e412a2903e79d44ec85484962a86
Anas <anas.shekho@gmail.com>
2021-01-26 19:14:47 +0100

    - added CRUD inventory - added Authentication service

--- volumes/back/authentication/models.py
+++ volumes/back/authentication/models.py
@@ -16,6 +16,12 @@ class UserModel(db.Model):
 
     password = db.Column(db.String(120), nullable=False)
 
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+    isAdmin = db.Column(db.Bool, nullable=False, default=False)
+
+    address = db.Column(db.Text, nullable=False)
+
     """
     Save user details in Database
     """
