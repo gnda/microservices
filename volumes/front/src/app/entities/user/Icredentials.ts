0c308ed01405af2ea13065271962d994c2e69f46
Laykreg <laykreg@gmail.com>
2021-02-08 22:32:01 +0100

    Working with notus-angular

--- volumes/front/src/app/entities/user/Icredentials.ts
+++
@@ -1,5 +0,0 @@
-export interface Icredentials {
-  login?: string;
-  password?: string;
-  rememberMe?: boolean;
-}
--- volumes/front/src/app/models/authentification.ts
+++
@@ -1,6 +0,0 @@
-export interface Authentification {
-    name: string,
-    password: string,
-    adresse: string,
-    admin: string
-  } // faire le bouton pour accéder à la page de connexion et la page de connexion
